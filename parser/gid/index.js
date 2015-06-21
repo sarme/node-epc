4a5a0ca345163b01fca9f6be8c2ad188e61dada1
sarme <sean.arme@gmail.com>
2015-06-21 12:59:36 -0500

    Added GID

---
+++ parser/gid/index.js
@@ -0,0 +1,96 @@
+'use strict';
+
+var TAG = 'parser.gid';
+var header = '00110101';
+
+var Abstract = require('../abstract'),
+	log = require('npmlog-ts'),
+	Q = require('q');
+
+var self = Object.create(Abstract, {
+	parse: {
+		value: function(val) {
+			return Q.Promise(function(resolve, reject) {
+				try {
+					log.verbose(TAG, 'parse');
+					log.info(TAG, 'parsing %j', val);
+
+					self.base.parse(val);
+
+					var bh = new self.base.bitsHelper(val, 96);
+
+					if (bh.bits.slice(0, 8) !== header)
+						throw new Error('Not a valid GID.');
+
+					self.parts.Header = bh.bits.slice(0, 8);
+					self.parts.ManagerNumber = parseInt(bh.bits.slice(8, 36), 2);
+					self.parts.ObjectClass = parseInt(bh.bits.slice(36, 60), 2);
+					self.parts.SerialNumber = parseInt(bh.bits.slice(60), 2);
+
+					resolve(self);
+				} catch (e) {
+					log.error(TAG, e);
+
+					reject(e);
+				}
+			});
+
+		}
+	},
+
+	canParse: {
+		value: function(val) {
+			return Q.Promise(function(resolve, reject) {
+				try {
+					log.verbose(TAG, 'canParse');
+
+					var bh = new self.base.bitsHelper(val, 96);
+
+					resolve(bh.bits.slice(0, 8) === header);
+				} catch (e) {
+					log.error(TAG, e);
+
+					reject(e);
+				}
+			});
+		}
+	},
+	getUri: {
+		value: function(val) {
+			return Q.Promise(function(resolve, reject) {
+				try {
+					log.verbose(TAG, 'getUri');
+
+					self.base.getUri(val);
+
+					self.parse(val)
+						.then(function(parsed) {
+							resolve('urn:epc:tag:gid:' + parsed.parts.ManagerNumber + '.' + parsed.parts.ObjectClass + '.' + parsed.parts.SerialNumber);
+						});
+				} catch (e) {
+					log.error(TAG, e);
+
+					reject(e);
+				}
+			});
+		}
+	},
+
+	getName: {
+		value: function() {
+			return Q.Promise(function(resolve, reject) {
+				try {
+					log.verbose(TAG, 'getName');
+
+					resolve('gid');
+				} catch (e) {
+					log.error(TAG, e);
+
+					reject(e);
+				}
+			});
+		}
+	}
+});
+
+module.exports = self;
---
+++ test/parser/gid.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var gid = require('../../parser/gid');
+
+describe('gid', function() {
+	it('canParse.  Failed.  Invalid format.', function() {
+		var p = gid.canParse('123456789012345678ABCDEF');
+
+		return p.should.become(false);
+	});
+
+	it('canParse', function() {
+		var p = gid.canParse('358000001800001800000001');
+
+		return p.should.become(true);
+	});
+
+	it('parse.  Failed.  Invalid format.', function() {
+		var p = gid.parse('123456789012345678ABCDEF');
+
+		return p.should.be.rejectedWith(Error);
+	});
+
+	it('parse', function() {
+		return gid.parse('358000001800001800000001')
+			.then(function(parsed) {
+				global.expect(parsed.parts.Header).to.equal('00110101', 'Invalid header');
+				global.expect(parsed.parts.ManagerNumber).to.equal(134217729, 'Invalid company prefix');
+				global.expect(parsed.parts.ObjectClass).to.equal(8388609, 'Invalid item reference');
+				global.expect(parsed.parts.SerialNumber).to.equal(34359738369, 'Invalid serial number');
+			});
+	});
+
+	it('getUri', function() {
+		var p = gid.getUri('358000001800001800000001');
+
+		return p.should.become('urn:epc:tag:gid:134217729.8388609.34359738369');
+	});
+
+	it('getName', function() {
+		var p = gid.getName()
+			.fail(function(err) {
+				throw err;
+			});
+
+		return p.should.become('gid');
+	});
+});

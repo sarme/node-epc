4bb22cdb55ae0ba0ba464459f4f8b996ba1f7065
sarme <sean.arme@gmail.com>
2015-06-21 11:05:59 -0500

    Added SGLN

---
+++ parser/sgln/index.js
@@ -0,0 +1,119 @@
+'use strict';
+
+var TAG = 'parser.sgln';
+var header = '00110010';
+var partition = {
+	bits: {
+		company: [40, 37, 34, 30, 27, 24, 20],
+		reference: [1, 4, 7, 11, 14, 17, 21]
+	},
+	digits: {
+		company: [12, 11, 10, 9, 8, 7, 6],
+		reference: [0, 1, 2, 3, 4, 5, 6]
+	}
+};
+
+var Abstract = require('../abstract'),
+	log = require('npmlog-ts'),
+	Q = require('q');
+
+var self = Object.create(Abstract, {
+	parse: {
+		value: function(val) {
+			return Q.Promise(function(resolve, reject) {
+				try {
+					log.verbose(TAG, 'parse');
+					log.info(TAG, 'parsing %j', val);
+
+					self.base.parse(val);
+
+					var bh = new self.base.bitsHelper(val, 96);
+
+					if (bh.bits.slice(0, 8) !== header)
+						throw new Error('Not a valid sgln.');
+
+					self.parts.Header = bh.bits.slice(0, 8);
+					self.parts.Filter = parseInt(bh.bits.slice(8, 11), 2);
+					self.parts.Partition = parseInt(bh.bits.slice(11, 14), 2);
+
+					var companyPrefixEnd = 14 + partition.bits.company[self.parts.Partition];
+
+					var company = parseInt(bh.bits.slice(14, companyPrefixEnd), 2).toString();
+					company = Array(partition.digits.company[self.parts.Partition] - company.length + 1).join('0') + company;
+
+					self.parts.CompanyPrefix = company;
+
+					var reference = parseInt(bh.bits.slice(companyPrefixEnd, companyPrefixEnd + partition.bits.reference[self.parts.Partition]), 2).toString();
+					reference = Array(partition.digits.reference[self.parts.Partition] - reference.length + 1).join('0') + reference;
+
+					self.parts.LocationReference = reference;
+
+					self.parts.Extension = parseInt(bh.bits.slice(55), 2);
+
+					resolve(self);
+				} catch (e) {
+					log.error(TAG, e);
+
+					reject(e);
+				}
+			});
+
+		}
+	},
+
+	canParse: {
+		value: function(val) {
+			return Q.Promise(function(resolve, reject) {
+				try {
+					log.verbose(TAG, 'canParse');
+
+					var bh = new self.base.bitsHelper(val, 96);
+
+					resolve(bh.bits.slice(0, 8) === header);
+				} catch (e) {
+					log.error(TAG, e);
+
+					reject(e);
+				}
+			});
+		}
+	},
+	getUri: {
+		value: function(val) {
+			return Q.Promise(function(resolve, reject) {
+				try {
+					log.verbose(TAG, 'getUri');
+
+					self.base.getUri(val);
+
+					self.parse(val)
+						.then(function(parsed) {
+							resolve('urn:epc:tag:sgln:' + parsed.parts.CompanyPrefix + '.' + parsed.parts.LocationReference + '.' + parsed.parts.Extension);
+						});
+				} catch (e) {
+					log.error(TAG, e);
+
+					reject(e);
+				}
+			});
+		}
+	},
+
+	getName: {
+		value: function() {
+			return Q.Promise(function(resolve, reject) {
+				try {
+					log.verbose(TAG, 'getName');
+
+					resolve('sgln');
+				} catch (e) {
+					log.error(TAG, e);
+
+					reject(e);
+				}
+			});
+		}
+	}
+});
+
+module.exports = self;
---
+++ test/parser/sgln.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var sgln = require('../../parser/sgln');
+
+describe('sgln', function() {
+	it('canParse.  Failed.  Invalid format.', function() {
+		var p = sgln.canParse('123456789012345678ABCDEF');
+
+		return p.should.become(false);
+	});
+
+	it('canParse', function() {
+		var p = sgln.canParse('323407890009A50000000001');
+
+		return p.should.become(true);
+	});
+
+	it('parse.  Failed.  Invalid format.', function() {
+		var p = sgln.parse('123456789012345678ABCDEF');
+
+		return p.should.be.rejectedWith(Error);
+	});
+
+	it('parse', function() {
+		return sgln.parse('323407890009A50000000001')
+			.then(function(parsed) {
+				global.expect(parsed.parts.Header).to.equal('00110010', 'Invalid header');
+				global.expect(parsed.parts.Filter).to.equal(1, 'Invalid filter');
+				global.expect(parsed.parts.Partition).to.equal(5, 'Invalid partition');
+				global.expect(parsed.parts.CompanyPrefix).to.equal('0123456', 'Invalid company prefix');
+				global.expect(parsed.parts.LocationReference).to.equal('01234', 'Invalid location reference');
+				global.expect(parsed.parts.Extension).to.equal(1099511627777, 'Invalid extension');
+			});
+	});
+
+	it('getUri', function() {
+		var p = sgln.getUri('323407890009A50000000001');
+
+		return p.should.become('urn:epc:tag:sgln:0123456.01234.1099511627777');
+	});
+
+	it('getName', function() {
+		var p = sgln.getName()
+			.fail(function(err) {
+				throw err;
+			});
+
+		return p.should.become('sgln');
+	});
+});
